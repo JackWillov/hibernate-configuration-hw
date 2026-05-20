e067b170f7b87f15a574d0b719e9d6808b9f91c2
JackWillov <popovuch02@gmail.com>
2026-05-20 04:28:50 +0200

    first try with functionality test error

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,20 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
+
     public static void main(String[] args) {
 
+        MovieService movieService = (MovieService) Injector
+                .getInstance("mate.academy")
+                .getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setDescription("second part of magic");
+        movie.setTitle("Harry Potter 3");
+        movieService.add(movie);
+        System.out.println(movieService.get(1L).getTitle());
     }
 }
---
+++ src/main/java/mate/academy/service/MovieServiceImpl.java
@@ -0,0 +1,27 @@
+package mate.academy.service;
+
+import mate.academy.dao.MovieDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.Movie;
+
+@Service
+public class MovieServiceImpl implements MovieService {
+
+    @Inject
+    private MovieDao movieDao;
+
+    @Override
+    public Movie add(Movie movie) {
+        if (movie.getId() != 0) {
+            throw new DataProcessingException("Id can't be picked by user");
+        }
+        return movieDao.add(movie);
+    }
+
+    @Override
+    public Movie get(Long id) {
+        return movieDao.get(id).orElse(null);
+    }
+}
